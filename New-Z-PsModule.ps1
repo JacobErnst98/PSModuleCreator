fd6cd8d35670b4575f121d106f45645919513bea
Fischer <zachary.fischer@sunydutchess.edu>
2018-10-31 17:46:59 -0400

    initial commit

--- New-Z-PsModule.ps1
+++ New-Z-PsModule.ps1
@@ -1,6 +1,19 @@
-﻿function New-Z-PSModule(){
-param(
+﻿#builds a PS Module
+function New-Z-PSModule(){
+    param(
+        [Parameter(mandatory=$true)][string]$source,
+        [Parameter(mandatory=$true)][string]$OutPath,
+        [Parameter(mandatory=$true)][string]$ModuleName,
+        [Parameter(mandatory=$true)][String]$Author,
+        [Parameter(mandatory=$true)][string]$description
+    )
 
-)
+mkdir $Path\$ModuleName
+mkdir $Path\$ModuleName\Private
+mkdir $Path\$ModuleName\Public
+mkdir $Path\$ModuleName\en-US # For about_Help files
+mkdir $Path\Tests
+
+
 
 }
