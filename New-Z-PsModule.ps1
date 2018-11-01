23f754e3fb7a9eabb54411748dc278fc2c3deb79
Jacob Ernst <Jacobaernst@gmail.com>
2018-11-01 09:14:32 -0400

    Added get-functions

--- New-Z-PsModule.ps1
+++ New-Z-PsModule.ps1
@@ -23,6 +23,15 @@ function New-GitRepo(){
         [Parameter(mandatory=$true)][string]$token,
         [Parameter(mandatory=$true)][string]$name,
     )
+    # https://github.com/settings/tokens/new
  curl https://api.github.com/user/repos?access_token=$token -d '{"name":"$name"}'
 
+}
+
+function Get-functions(){
+   [Parameter(mandatory=$true)][string]$file
+    $oldarray= Get-ChildItem function:\
+    import-module $file
+    $newarray= Get-ChildItem function:\
+    return ($newarray | where {$oldarray -notcontains $_}).name
 }
