ea304afddabe455c77b49e66d3059f7e08ca1f17
Mentaleak <mentaleak@gmail.com>
2018-11-06 18:00:46 -0500

    Modified Convertto-PsModule.ps1
    Content Modified

--- Convertto-PsModule.ps1
+++ Convertto-PsModule.ps1
@@ -1,54 +1,45 @@
 ﻿#builds a PS Module
-function New-Z-PSModule(){
+#requires PSGit
+
+function ConvertTo-PSModule(){
     param(
-        [Parameter(mandatory=$true)][string]$source,
-        [Parameter(mandatory=$true)][string]$OutPath,
-        [Parameter(mandatory=$true)][string]$ModuleName,
-        [Parameter(mandatory=$true)][String]$Author,
-        [Parameter(mandatory=$true)][string]$description
+        [Parameter(mandatory=$true)][string]$source
     )
+    if(!(Test-GitAuth -nobreak)){	
+        connect-github
+    }
+    if(Test-GitAuth){
+        $SourceFile=(Get-Item $source)
+        $ProjectPath=(Get-Item $source).Directory.FullName
+        $userData=get-gituserdata
 
-mkdir $Path\$ModuleName
-mkdir $Path\$ModuleName\Private
-mkdir $Path\$ModuleName\Public
-mkdir $Path\$ModuleName\en-US # For about_Help files
-mkdir $Path\Tests
+        #Write-Host "Test Local"
+        if (test-GitLocal -ProjectPath $ProjectPath) {
+		#Write-Host "Test remote"
+		    if (test-GitRemote -ProjectPath $ProjectPath) {
+                $RepoData=get-gitrepo -ProjectPath $ProjectPath
 
-
-
-}
-
-function New-GitRepo(){
-    param(
-        [Parameter(mandatory=$true)][string]$token,
-        [Parameter(mandatory=$true)][string]$name,
-    )
-    # https://github.com/settings/tokens/new
- curl https://api.github.com/user/repos?access_token=$token -d '{"name":"$name"}'
-
-}
-
-function Get-functions(){
-   [Parameter(mandatory=$true)][string]$file
-    $oldarray= Get-ChildItem function:\
-    import-module $file
-    $newarray= Get-ChildItem function:\
-    return ($newarray | where {$oldarray -notcontains $_}).name
-}
+                $ModuleName=$RepoData.name
 
 
-function New-GitToken() {
- [Parameter(mandatory=$true)][string]$user
-  [Parameter(mandatory=$true)][string]$pass
 
-$pair = "${user}:${pass}"
-$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
-$base64 = [System.Convert]::ToBase64String($bytes)
-$basicAuthValue = "Basic $base64"
-$headers = @{ Authorization = $basicAuthValue}
-$postparams = @{scopes = "repo";note = "ZPSMODULE" }
-Invoke-WebRequest -uri "https://api.github.com/authorizations" -Headers $headers -Method POST -body $postparams
+                #make directory for module
+                <#
+                New-Item -ItemType directory -Path "$($SourceFile.Directory.FullName)\Module"
+                New-Item -ItemType directory -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)"
+                New-Item -ItemType directory -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)\Private"
+                New-Item -ItemType directory -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)\Public"
+                New-Item -ItemType directory -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)\en-US"
 
-#curl -u '$username'  -d '{"scopes":["repo"],"note":"ZPSMODULE"}' https://api.github.com/authorizations
-#Curl -u does not exsist in powershell we will have to find a way to do this with invoke-webrequest
+               
+                New-ModuleManifest -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)\$($ModuleName).psd1" `
+                                   -RootModule $ModuleName.psm1 `
+                                   -Description $Description `
+                                   -PowerShellVersion 3.0 `
+                                   -Author $Author `
+                                   -FormatsToProcess "$ModuleName.Format.ps1xml"
+               #>
+             }
+        }                      
+    }
 }
