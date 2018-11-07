d43d193e6673e43271f1fc24b8d749eb7f948845
Mentaleak <mentaleak@gmail.com>
2018-11-07 13:47:50 -0500

    Modified Convertto-PsModule.ps1
    Content Modified

--- Convertto-PsModule.ps1
+++ Convertto-PsModule.ps1
@@ -1,5 +1,6 @@
 ﻿#builds a PS Module
 #requires PSGit
+#requires PSTools
 
 function ConvertTo-PSModule(){
     param(
@@ -20,6 +21,85 @@ function ConvertTo-PSModule(){
                 $RepoData=get-gitrepo -ProjectPath $ProjectPath
 
                 $ModuleName=$RepoData.name
+                
+
+
+                $authors=($RepoData.contributors_stats |Format-Table -autosize |Out-String)
+
+
+
+
+<# 
+# RootModule = ''
+
+# Author of this moduleAuthor = 'User01'
+
+# Description of the functionality provided by this module
+# Description = ''
+
+# Version number of this module.ModuleVersion = '1.0'
+
+# HelpInfo URI of this module
+# HelpInfoURI = '
+#>
+
+
+# ID used to uniquely identify this moduleGUID = 'd0a9150d-b6a4-4b17-a325-e3a24fed0aa9'
+# Company or vendor of this moduleCompanyName = 'Unknown'
+# Copyright statement for this moduleCopyright = '(c) 2012 User01. All rights reserved.'
+# Minimum version of the PowerShell engine required by this module
+
+# PowerShellVersion = ''
+# Name of the PowerShell host required by this module
+# PowerShellHostName = ''
+# Minimum version of the PowerShell host required by this module
+# PowerShellHostVersion = ''
+
+# Minimum version of the .NET Framework required by this module
+# DotNetFrameworkVersion = ''
+# Minimum version of the common language runtime (CLR) required by this module
+# CLRVersion = ''
+
+# Processor architecture (None, X86, Amd64) required by this module
+# ProcessorArchitecture = ''
+
+# Modules that must be imported into the global environment prior to importing this module
+# RequiredModules = @()
+
+# Assemblies that must be loaded prior to importing this module
+# RequiredAssemblies = @()
+
+# Script files (.ps1) that are run in the caller's environment prior to importing this module
+# ScriptsToProcess = @()
+
+# Type files (.ps1xml) to be loaded when importing this module
+# TypesToProcess = @()
+
+# Format files (.ps1xml) to be loaded when importing this module
+# FormatsToProcess = @()
+
+# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+# NestedModules = @()
+
+# Functions to export from this moduleFunctionsToExport = '*'
+
+# Cmdlets to export from this moduleCmdletsToExport = '*'
+# Variables to export from this moduleVariablesToExport = '*'
+# Aliases to export from this moduleAliasesToExport = '*'
+
+# List of all modules packaged with this module# ModuleList = @()
+# List of all files packaged with this module
+# FileList = @()
+# Private data to pass to the module specified in RootModule/ModuleToProcess
+# PrivateData = ''
+
+# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+# DefaultCommandPrefix = ''}
+
+
+
+
+
 
 
 
@@ -34,9 +114,12 @@ function ConvertTo-PSModule(){
                
                 New-ModuleManifest -Path "$($SourceFile.Directory.FullName)\Module\$($ModuleName)\$($ModuleName).psd1" `
                                    -RootModule $ModuleName.psm1 `
-                                   -Description $Description `
+                                   -Description $RepoData.Description `
+                                   -Author $authors `
+                                   -Version $RepoData.updated_at `
+                                   -HelpInfoURI $RepoData.html_url `
+
                                    -PowerShellVersion 3.0 `
-                                   -Author $Author `
                                    -FormatsToProcess "$ModuleName.Format.ps1xml"
                #>
              }
