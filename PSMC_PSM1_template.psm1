7efebc1ef3c719afc0ea95bd09dfee57bfce12ac
Mentaleak <mentaleak@gmail.com>
2018-11-26 17:48:16 -0500

    Modified PSMC_PSM1_template.psm1
    Content Modified

--- PSMC_PSM1_template.psm1
+++ PSMC_PSM1_template.psm1
@@ -1,7 +1,4 @@
-﻿<#{.ModuleName.}#>
-<#{.PSVersionREQ.}#>
-
-param([switch]$NoVersionCheck)
+﻿param([switch]$NoVersionCheck)
 
 #Is module loaded; if not load
 if (!(Get-Module <#{.ModuleName.}#>)){ 
@@ -13,19 +10,3 @@ if (!(Get-Module <#{.ModuleName.}#>)){
         "Visit Microsoft to download the latest Windows Management Framework `n" +
         "To suppress this warning, change your include to 'Import-Module <#{.ModuleName.}#> -NoVersionCheck `$true'.")
     }
-    else{
-        #get functions
-        $PrivateFunctions =Get-childItem -path "$($PSScriptRoot)\Private" -Filter "*.ps1"
-        $PublicFunctions =Get-childItem -path "$($PSScriptRoot)\Public" -Filter "*.ps1"
-        foreach($PrivateFunction in $PrivateFunctions)
-        {
-            . $PrivateFunction.FullName
-        }
-        foreach($PublicFunction in $PublicFunctions)
-        {
-            . $PublicFunction.FullName
-            Export-ModuleMember $PublicFunction.Name
-        }
-
-    }
-}
