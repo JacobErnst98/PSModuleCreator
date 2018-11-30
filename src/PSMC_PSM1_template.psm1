410520f2640b6353de8c40d12b5b084ce5fcf29d
Mentaleak <mentaleak@gmail.com>
2018-11-30 17:52:50 -0500

    Added src/PSMC_PSM1_template.psm1
    Content Modified

---
+++ src/PSMC_PSM1_template.psm1
@@ -0,0 +1,13 @@
+﻿param([switch]$NoVersionCheck)
+
+#Is module loaded; if not load
+if ((Get-Module <#{.ModuleName.}#>)){return}
+    $psv = $PSVersionTable.PSVersion
+
+    #verify PS Version
+    if ($psv.Major -lt <#{.PSVersionREQ.}#> -and !$NoVersionWarn) {
+        Write-Warning ("<#{.ModuleName.}#> is listed as requiring <#{.PSVersionREQ.}#>; you have version $($psv).`n" +
+        "Visit Microsoft to download the latest Windows Management Framework `n" +
+        "To suppress this warning, change your include to 'Import-Module <#{.ModuleName.}#> -NoVersionCheck `$true'.")
+        return
+    }
